1d9089865a10763ad1276a06a6b7c1b2f33d85e6
jpbmoura <jopmoura49@gmail.com>
2024-07-08 22:17:37 -0300

    add vercel.json

--- src/components/global/layout/index.tsx
+++ src/components/global/layout/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../header";
-import Footer from "../footer";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -8,12 +7,12 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex flex-col justify-between h-screen">
+    <div className="flex flex-col justify-between">
       <Header />
       <div className="max-w-3xl w-full m-auto px-10 md:px-0 ">
         <main>{children}</main>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
