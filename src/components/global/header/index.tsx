430d6e8e52089caa923c0b70fb7c2fe1ed053b08
jpbmoura <jopmoura49@gmail.com>
2024-07-10 22:34:36 -0300

    change redirect on contact button

--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -13,6 +13,9 @@ import {
 import CategoriesSheet from "../../shared/categories-sheet";
 
 const Header = () => {
+  const socialLink =
+    "https://linktr.ee/paubrasilatacado?fbclid=PAZXh0bgNhZW0CMTEAAaaltjuhg1OEzAamOSwNgEJ9HTcch7aXqXklYSCGgu4D8YLESZQHe35RH0s_aem_Nsjoj3Eg7HDT-Czv1ytacw";
+
   return (
     <header
       className={`py-4 border-b dark:border-neutral-700 bg-white sticky top-0 z-10 shadow-md px-10 md:px-0`}
@@ -39,38 +42,12 @@ const Header = () => {
             </SheetContent>
           </Sheet>
 
-          <Popover>
-            <PopoverTrigger className="hidden md:inline-flex">
-              <a
-                href="#"
-                className="inline-flex h-9 items-center justify-center rounded-md bg-green-700 px-4 py-2 text-sm font-medium text-white shadow transition-colors hover:bg-green-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-              >
-                Fale Conosco!
-              </a>
-            </PopoverTrigger>
-            <PopoverContent align="end" className="w-48">
-              <div>
-                <span className="group">
-                  <a
-                    href="#"
-                    className="flex flex-row items-center gap-3 w-full py-2 px-4 text-sm font-medium text-neutral-900 hover:bg-neutral-100"
-                  >
-                    <ChatBubbleIcon className="size-5 group-hover:text-green-600 transition-colors" />
-                    Whatsapp
-                  </a>
-                </span>
-                <span className="group">
-                  <a
-                    href="#"
-                    className="flex flex-row items-center gap-3 w-full py-2 px-4 text-sm font-medium text-neutral-900 hover:bg-neutral-100"
-                  >
-                    <InstagramLogoIcon className="size-5 group-hover:text-purple-500 transition-colors" />
-                    Instagram
-                  </a>
-                </span>
-              </div>
-            </PopoverContent>
-          </Popover>
+          <a
+            href={socialLink}
+            className="inline-flex h-9 items-center justify-center rounded-md bg-green-700 px-4 py-2 text-sm font-medium text-white shadow transition-colors hover:bg-green-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+          >
+            Fale Conosco!
+          </a>
         </div>
       </div>
     </header>
